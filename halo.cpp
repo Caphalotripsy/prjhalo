2a014532bf409644607170150fd797f5a2b29bdb
Caphalotripsy <imamnawwi123@gmail.com>
2025-05-22 15:06:11 +0800

    Penambahan fungsi jumlah nama yang akan ditambahkan

--- halo.cpp
+++ halo.cpp
@@ -3,9 +3,18 @@
 using namespace std;
 
 int main () {
+    /*
     cout << "Poliban juara";
     cout << "Barca Juara ";
     cout << "Perubahan 1";
     cout << "Masih perubahan 1 tapi tetap biar ada varian aja";
+    */
+    int jumlah;
+
+    //Jumlah nama yang akan dimasukkan
+    cout << "Masukkan jumlah nama yang ingin dimasukkan: ";
+    cin >> jumlah;
+
+    
     return 0;
 }
