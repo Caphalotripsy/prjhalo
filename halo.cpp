307ae5586887f3684625558d6d53c41fea5e6aec
Caphalotripsy <imamnawwi123@gmail.com>
2025-05-22 14:39:57 +0800

    Perubahan 1

--- halo.cpp
+++ halo.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 
 int main () {
-
-    
+    cout << "Poliban juara";
+    cout << "Barca Juara ";
+    cout << "Perubahan 1";
+    cout << "Masih perubahan 1 tapi tetap biar ada varian aja";
+    return 0;
 }
